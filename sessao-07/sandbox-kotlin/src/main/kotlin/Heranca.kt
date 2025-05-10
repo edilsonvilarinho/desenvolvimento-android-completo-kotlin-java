b1a642931e7bc2a1d51b7301b9e8b4f565cf692f
edilsonvilarinho <edilson.vilarinho.messias@gmail.com>
2025-05-10 02:55:55 -0300

    add:Interface

--- sessao-07/sandbox-kotlin/src/main/kotlin/Heranca.kt
+++ sessao-07/sandbox-kotlin/src/main/kotlin/Heranca.kt
@@ -2,25 +2,27 @@ package org.example
 
 import kotlin.math.sqrt
 
-abstract class Area {
-    abstract fun calular(): Number
+interface Calcular {
+    fun calcular(): Number
 }
 
+abstract class Area
 
-class Quadrado(var lado: Double) : Area() {
-    override fun calular(): Number {
+
+class Quadrado(var lado: Double) : Area(), Calcular {
+    override fun calcular(): Number {
         return sqrt(lado)
     }
 }
 
-class Triangulo(var base: Double, var altura: Double) : Area() {
-    override fun calular(): Number {
+class Triangulo(var base: Double, var altura: Double) : Area(), Calcular {
+    override fun calcular(): Number {
         return base * altura / 2
     }
 }
 
 
 fun main() {
-    println(Quadrado(1.0).calular())
-    println(Triangulo(1.5 , 2.0).calular())
+    println(Quadrado(1.0).calcular())
+    println(Triangulo(1.5, 2.0).calcular())
 }
