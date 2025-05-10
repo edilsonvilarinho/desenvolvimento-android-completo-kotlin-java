98bf5a06e532d1f386e4cf556cb5a0decaf411a1
edilsonvilarinho <edilson.vilarinho.messias@gmail.com>
2025-05-10 02:34:17 -0300

    add: Sobrecarga de métodos

---
+++ sessao-07/sandbox-kotlin/src/main/kotlin/SobrecargaMetodos.kt
@@ -0,0 +1,14 @@
+package org.example
+
+class Logar {
+    
+    fun logar(email: String, senha: String) = println("logou email = [${email}], senha = [${senha}]")
+
+    fun logar(numeroTelefone: String) = println("logou email = [${numeroTelefone}]")
+
+}
+
+
+fun main() {
+
+}
