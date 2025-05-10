75b6b3ea7c7f12a334a7406972824c99af7e375a
edilsonvilarinho <edilson.vilarinho.messias@gmail.com>
2025-05-10 01:54:08 -0300

    add:Variáveis e constantes

---
+++ sessao-07/sandbox-kotlin/src/main/kotlin/VariaveisConstantes.kt
@@ -0,0 +1,16 @@
+package org.example
+
+import java.math.BigDecimal
+
+const val OK = "OK"
+
+fun main() {
+    val variavel = 0
+    val constante = 0
+    var int: Int
+    var string: String
+    var float: Float
+    var bigDecimal: BigDecimal
+    var boolean: Boolean
+    var number: Number
+}
